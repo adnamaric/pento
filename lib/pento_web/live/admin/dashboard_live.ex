0c67a4ceb84ae2d7dd286b0447c4b143bfda581b
adnamaric <adna.maric@edu.fit.ba>
2022-05-29 12:26:05 +0200

    add user activity

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -1,9 +1,24 @@
 defmodule PentoWeb.Admin.DashboardLive do
     use PentoWeb, :live_view
-  
+    #alias PentoWeb.{SurveyResultsLive, UserActivityLive, ProductSalesLive}
+    alias PentoWeb.Endpoint
+
+    @survey_results_topic "survey_results"
+    @user_activity_topic "user_activity"
+ 
+    
     def mount(_params, _session, socket) do
+    
       {:ok,
        socket
-       |> assign(:survey_results_component_id, "survey-results")}
+       |> assign(:survey_results_component_id, "survey-results")
+       |> assign(:user_activity_component_id, "user-activity")}
     end
+    
+    def handle_info(%{event: "presence_diff"}, socket) do
+      send_update(
+      UserActivityLive,
+      id: socket.assigns.user_activity_component_id)
+      {:noreply, socket}
+      end
   end
